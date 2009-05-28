c276de76898a662ee0c4c66b7e5459a43503c8c3
Indrek Juhkam <indrek@urgas.eu>
2009-05-28 18:46:49 +0300

    Oops

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -11,7 +11,7 @@ class Application < Merb::Controller
   private
 
   def check_admin
-    return if params[:pw].blank? || !admin?
+    return if params[:pw].blank? || admin?
 
     pw = "f96679019bbe38d09fea7b103e4fea741a7c9d97f03264ae05317b0efee727d4446f969652ead86b75becc95c739e5ef881aab492a262c5043d084a20ac4e775"
     if Digest::SHA512.new.update(params[:pw]).to_s == pw
