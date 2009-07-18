6f6e0a811441405d900b6f5519379025b9a9ff3d
Indrek Juhkam <indrek@urgas.eu>
2009-07-18 21:34:31 +0300

    Bit better sql query.

--- app/controllers/assets.rb
+++ app/controllers/assets.rb
@@ -6,7 +6,7 @@ class Assets < Application
   # GET /assets
   def index
     @asset ||= Asset.new
-    @assets = Asset.find(:all, :order => 'LOWER(category) ASC, year DESC, LOWER(title) ASC')
+    @assets = Asset.find(:all, :select => 'id, title, category, author, year', :order => 'LOWER(category) ASC, year DESC, LOWER(title) ASC')
     @categories = @assets.map(&:category).map(&:downcase)
     render :template => 'assets/index'
   end
