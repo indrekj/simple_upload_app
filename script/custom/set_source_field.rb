f1e133fc10f8860ce9f73bc86c9581bca7d1a7e5
Indrek Juhkam <indrek@urgas.eu>
2010-02-10 19:04:03 +0200

    A little script to populare source fields

---
+++ script/custom/set_source_field.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require File.dirname(__FILE__) + "/../../config/environment"
+
+puts "Started!"
+
+Asset.all.each do |asset|
+  asset.determine_source!
+  asset.remove_delicate_info!
+  if asset.save
+    puts "Asset ##{asset.id} saved"
+  else
+    puts "ERROR: Unable to save asset ##{asset.id}"
+  end
+end
+
+puts "Finished!"
