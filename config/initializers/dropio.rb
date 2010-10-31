bc3598068da7677df04b266702422da1c5dc4f26
Indrek Juhkam <indrek@urgas.eu>
2010-10-31 23:37:11 +0200

    remove assets. there is no dropio any more.

--- config/initializers/dropio.rb
+++
@@ -1,6 +0,0 @@
-# http://backbonedocs.drop.io/Ruby-API-Client-Library
-config = YAML.load_file(Rails.root.to_s + "/config/dropio.yml")
-
-Dropio::Config.api_key = config[:key]
-Dropio::Config.api_secret = config[:secret]
-Dropio::Config.version = "3.0"
